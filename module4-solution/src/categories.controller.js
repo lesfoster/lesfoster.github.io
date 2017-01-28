5b2cb2dda67a44a05ead1dee4a2930c6a69b60e8
lesfoster <lfoster.se.be@gmail.com>
2017-01-28 17:02:02 -0500

    Working through the controller function after bypassing categories.  Still not giving us the data, but we can see that the controller is actually being used.

--- module4-solution/src/categories.controller.js
+++ module4-solution/src/categories.controller.js
@@ -4,12 +4,15 @@
     angular.module('Categories')
         .controller('CategoryController', CategoryController);
 
-    // ERROR: as of now, the injecter is not able to resolve category data's "provider'.
+    // ERROR: as of now, the injecter is not able to resolve category data's 'provider'.
     //   unknown fix.
-    CategoryController.$inject = ['categoryData'];
-    function CategoryController(categoryData) {
+    // CategoryController.$inject = ['categoryData'];
+//    function CategoryController(categoryData) {
+    CategoryController.$inject = [];
+    function CategoryController() {
         var catctrl = this;
-        catctrl.categories = categoryData;
+        catctrl.categories = {}; // categoryData;
+        catctrl.myTitle = 'This Stuff';
         console.log("Got categories");
     }
 
