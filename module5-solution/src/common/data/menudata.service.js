4eceebb4ccde4e8fa8d1025d18569bbc984993f8
lesfoster <lfoster.se.be@gmail.com>
2017-02-09 23:05:41 -0500

    Added data feed.

---
+++ module5-solution/src/common/data/menudata.service.js
@@ -0,0 +1,36 @@
+/**
+ * Created by Leslie L Foster on 2/9/2017.
+ */
+(function () {
+
+    angular.module('Data')
+        .service('MenuDataService', MenuDataService);
+
+    MenuDataService.$inject = ['$http'];
+    function MenuDataService ($http) {
+        var service = this;
+        service.getAllCategories = function() {
+            console.log("Getting categories via http.");
+            return $http( {
+                url: 'https://davids-restaurant.herokuapp.com/categories.json',
+                method: 'GET'
+            }).then(
+                function(result) {
+                    // Sharing this result.
+                    console.log("Returning from URL.");
+                    //$parent.result = result;
+                    return result;
+                }
+            );
+        }
+
+        service.getItemsForCategory = function(categoryShortName) {
+            return $http( {
+                params: {"category": categoryShortName},
+                url:    "https://davids-restaurant.herokuapp.com/menu_items.json?category=:categoryShortName",
+                method: 'GET'
+            })
+        }
+    }
+
+})();
